2000dc23b4bab37c1338a05194df10904a245d18
JuanitaAK <afanadorjuanita@gmail.com>
2023-03-12 23:06:02 +0100

    added img to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,9 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo">
-      <img src="./ball.png" alt="pikachu" />
+      <img src={"./pika.gif"} alt="pokemon-gif" />
       <h1 className="title">POKEDEX</h1>
+      <img src="./ball.png" alt="pikachu" />
     </div>
   );
 };
