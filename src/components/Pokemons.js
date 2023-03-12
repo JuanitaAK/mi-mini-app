9daf832db52072c10e5ccebd73ca7d14bef14b28
JuanitaAK <afanadorjuanita@gmail.com>
2023-03-12 13:47:18 +0100

    started select options

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -3,8 +3,26 @@ import axios from "axios";
 import Card from "./Card";
 
 const Pokemons = () => {
+  const types = [
+    "fire",
+    "grass",
+    "poison",
+    "bug",
+    "water",
+    "flying",
+    "electric",
+    "ground",
+    "fairy",
+    "ghost",
+    "ice",
+    "psychic",
+    "fighting",
+    "normal",
+    "dragon",
+  ];
   const [names, setName] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
+  const [selectedType, setSelectedType] = useState("");
 
   useEffect(() => {
     axios
@@ -16,12 +34,24 @@ const Pokemons = () => {
     <div className="pokemons">
       <ul className="radio-container">
         <input
+          className="input-range"
           type="range"
           min="1"
           max="250"
           defaultValue={rangeValue}
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setRangeValue(e.target.value)}
         />
+
+        <label id="selectedType">
+          Types :
+          <select onChange={(e) => setSelectedType(e.target.value)} id="type">
+            {types.map((type) => (
+              <option value={type} id={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </label>
       </ul>
       <ul className="poke">
         {names.slice(0, rangeValue).map((data) => (
